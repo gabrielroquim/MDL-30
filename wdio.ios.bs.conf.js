0d2eec8500e8b138ed9a919ec13d8f6887b02920
Gabriel Roquim <gabrielroquim@hotmail.com>
2023-02-12 12:38:09 -0300

    alterando hooks

--- wdio.ios.bs.conf.js
+++ wdio.ios.bs.conf.js
@@ -35,52 +35,7 @@ exports.config = {
     mochaOpts: {
         timeout: 300000
     },
-    reporters: ['spec',
-        ['allure', {
-            outputDir: 'allure-results',
-            disableWebdriverStepsReporting: true,
-            disableWebdriverScreenshotsReporting: true,
-        }],
-        [video, {
-            saveAllVideos: true,       // If true, also saves videos for successful test cases
-            videoSlowdownMultiplier: 50, // Higher to get slower videos, lower for faster videos [Value 1-100]
-        }]
-    ],
-    onComplete: function () {
-        const reportError = new Error('Could not generate Allure report')
-        const generation = allure(['generate', 'allure-results', '--clean'])
-        return new Promise((resolve, reject) => {
-            const generationTimeout = setTimeout(
-                () => reject(reportError),
-                5000)
-
-            generation.on('exit', function (exitCode) {
-                clearTimeout(generationTimeout)
-
-                if (exitCode !== 0) {
-                    return reject(reportError)
-                }
-
-                console.log('Allure report successfully generated')
-                resolve()
-            })
-        })
-    },
-    afterStep: function (test, scenario, { error, duration, passed }) {
-        if (error) {
-            driver.takeScreenshot()
-        }
-    },
-    beforeSuite: async function(){
-        //verificar se o app ja esta instalado e executando
-        let state = await driver.queryAppState("br.art.ebaconline")
-        if(state !== 4){
-            await driver.launchApp()
-        }
-    },
-    afterSuite: async function(){
-        //fechar o app
-        await driver.closeApp()
-    },
+   
+   
     maxInstances: 1
 }
